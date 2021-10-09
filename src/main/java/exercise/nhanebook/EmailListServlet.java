ca7b9cf83c76b5422f6611251bc4e556832051fb
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2021-10-09 16:17:10 +0700

    10/09/2021 Heroku Host

---
+++ src/main/java/exercise/nhanebook/EmailListServlet.java
@@ -0,0 +1,56 @@
+package exercise.nhanebook;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+//@WebServlet(name = "EmailListServlet", value = "/EmailListServlet")
+public class EmailListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = "/exercise.jsp";
+        String action = request.getParameter("action");
+
+        // get current action
+        if(action == null){
+            action = "join";
+        }
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/exercise.jsp";    // the "join" page
+        }
+        else if (action.equals("add")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object
+            User user = new User(firstName, lastName, email);
+
+            // validate the parameters
+            String message;
+            if (firstName == null || lastName == null || email == null ||
+                    firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                message = "Please fill out all three text boxes.";
+                url = "/exercise.jsp";
+            }
+            else {
+                message = "";
+                url = "/thanks.jsp";
+                UserDB.insert(user);
+            }
+            request.setAttribute("user", user);
+            request.setAttribute("message", message);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+}
---
+++ target/nhanebook-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,48 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>HomePage</title>
+</head>
+<body>
+<div class="container">
+    <div class="navbar">
+        <img class ="logo" alt="" src="image/bookshoplogo.png">
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Collections</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="exercise.jsp" >Exercise</a></li>
+                <li><a href="login.jsp">Sign Up</a></li>
+            </ul>
+            <ul>
+
+            </ul>
+        </nav>
+        <img src="image/cart.png" class="cart">
+    </div>
+    <div class="content">
+        <a href="" class="btn">2021 Best Sellers</a>
+        <h1>Be smarter <br> and wiser</h1>
+        <p>Find the best book at N&T's Online Bookstore</p>
+        <div class="arrow-icons">
+            <img src="image/back-arrow.png">
+            <img src="image/next-arrow.png">
+        </div>
+    </div>
+    <img src="image/img.png" class="feature-img">
+
+    <div class="social-links">
+        <a href="">FACEBOOK</a>
+        <a href="">INSTAGRAM</a>
+        <a href="">TWITTER</a>
+    </div>
+</div>
+</body>
+
+</html>
